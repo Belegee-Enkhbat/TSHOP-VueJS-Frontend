dd41ac70077d9d1c559c15a2568d11572206acca
uranbileg <uranbileg.enkhjargal@gmail.com>
2024-05-18 13:16:43 +0800

    fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,41 @@
 <template>
   <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>TSHOP</title>
-      <link rel="stylesheet" href="css/style.min.css" />
-    </head>
-    <body>
-      <main class="main"></main>
-    </body>
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TSHOP</title>
+    <link rel="stylesheet" href="css/style.min.css" />
+  </head>
+
+  <body>
+  <div class="container">
+    <h1>Зурвас илгээх</h1>
+    <form id="contactForm" @submit.prevent="submitForm">
+      <div>
+        <label for="name">Нэр:</label>
+        <input type="text" id="name" v-model="name" required>
+      </div>
+      <div>
+        <label for="email">И-мейл:</label>
+        <input type="email" id="email" v-model="email" required>
+      </div>
+      <div>
+        <label for="message">Сэтгэгдэл:</label>
+        <textarea id="message" v-model="message" required></textarea>
+      </div>
+      <button type="submit">Илгээх</button>
+    </form>
+    <div class="contact-info">
+      <h2>Холбоо барих мэдээлэл:</h2>
+      <p>Утас: +1234567890</p>
+      <p>И-мейл: info@example.com</p>
+      <p>Хаяг: Улаанбаатар, Монгол улс</p>
+    </div>
+  </div>
+  </body>
+
   </html>
 </template>
 
